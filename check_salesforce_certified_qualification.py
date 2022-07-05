57d828115b7db01b4d99ada4004753ae09e31a12
Shun <tanakagoro0319@yahoo.ne.jp>
2022-07-05 15:47:21 +0900

    ドライバーの指定方法変更

--- check_salesforce_certified_qualification.py
+++ check_salesforce_certified_qualification.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from urllib import request
 
-CHROME_DRIVER = webdriver.Chrome(ChromeDriverManager().install())
+CHROME_DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 URL = 'https://tandc.salesforce.com/certificate-holder'
 
 
